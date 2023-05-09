69ad120c4deabb0106deb9566fca267085bbe983
Xeito <74165796+Xeito21@users.noreply.github.com>
2023-05-09 20:46:10 +0700

    Putt,powerup,uipower

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BallController : MonoBehaviour
 {
     public float maxPower;
     public float changeAngleSpeed;
     public float lineLength;
+    public Slider powerSlider;
+    public TextMeshProUGUI puttCountLabel;
 
     private LineRenderer line;
     private Rigidbody ball;
     private float angle;
+    private float powerUpTime;
+    private float power;
+    private int putts;
 
     private void Awake()
     {
@@ -31,7 +38,11 @@ public class BallController : MonoBehaviour
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-
+            Putt();
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PowerUp();
         }
         UpdateLinePositions();
     }
@@ -45,6 +56,23 @@ public class BallController : MonoBehaviour
     {
         line.SetPosition(0, transform.position);
         line.SetPosition(1, transform.position + Quaternion.Euler(0, angle, 0) * Vector3.forward * lineLength);
+    }
+
+    private void Putt()
+    {
+        ball.AddForce(Quaternion.Euler(0, angle, 0) * Vector3.forward * maxPower * power, ForceMode.Impulse);
+        power = 0;
+        powerSlider.value = 0;
+        powerUpTime = 0;
+        putts++;
+        puttCountLabel.text = putts.ToString();
+    }
+
+    private void PowerUp()
+    {
+        powerUpTime += Time.deltaTime;
+        power = Mathf.PingPong(powerUpTime, 1);
+        powerSlider.value = power;
     }
 
 }
